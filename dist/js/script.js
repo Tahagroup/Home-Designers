7a14b648b0c3a5789a5d4173be1b9e82e6981816
Archer <y.basiri@yahoo.com>
2022-09-26 19:23:37 +0330

    added drawer menu
    + added drawer menu
    + icons hover effect

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+// window.onload = function () {};
+// openDrawer();
+function openDrawer() {
+  const drawer = document.getElementsByClassName("drawer")[0];
+  drawer.style.width = "288px";
+}
+
+function closeDrawer() {
+  const drawer = document.getElementsByClassName("drawer")[0];
+  drawer.style.width = "0";
+}
